c0f4831a6c599fcd0861858799620e8330a17b7b
JulioQuispe-Chavez17 <julio.quispe@vallegrande.edu.pe>
2020-10-14 15:59:04 -0500

    Usando la librería turtle

---
+++ TRACK01/Function.py
@@ -0,0 +1,17 @@
+#COMMNET
+import turtle
+
+cesar_turtle = turtle.Turtle()
+def square():
+    cesar_turtle.forward(100)
+    cesar_turtle.right(90)
+    cesar_turtle.forward(100)
+    cesar_turtle.right(90)
+    cesar_turtle.forward(100)
+    cesar_turtle.right(90)
+    cesar_turtle.forward(100)
+
+square()
+cesar_turtle.forward(200)
+square()
+cesar_turtle.forward(200)
---
+++ TRACK01/Variable.py
@@ -0,0 +1,10 @@
+import turtle
+
+cesar_turtle = turtle.Turtle()
+cesar_turtle.forward(100)
+cesar_turtle.right(90)
+cesar_turtle.forward(100)
+cesar_turtle.right(90)
+cesar_turtle.forward(100)
+cesar_turtle.right(90)
+cesar_turtle.forward(100)
